21d28a0b634884fab1e9682f875ffd2ff6e41230
Romain Pesche <romain@pesche.fr>
2020-04-08 22:36:03 +0200

    add setup for heroku deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: cd werewolf/ && python manage.py migrate
+web: cd werewolf/ && gunicorn wsgi --log-file -
--- werewolf/werewolf/settings.py
+++ werewolf/werewolf/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$b%-#nsgp=r5ksx8vikm)e%#jeqz%3*xe_cr8v9uv!^p#=su%='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +76,7 @@ WSGI_APPLICATION = 'werewolf.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -118,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
