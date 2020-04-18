a3cf7b506e5948545526c81534d2dd7bb0f04802
Romain Pesche <romain@pesche.fr>
2020-04-18 23:03:27 +0200

    add test_settings.py settings for test purpose

---
+++ werewolf/werewolf/test_settings.py
@@ -0,0 +1,14 @@
+import os
+
+from werewolf.settings import *
+from werewolf.settings import BASE_DIR
+
+SECRET_KEY = "TestSecretKey"
+
+
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        }
+}
