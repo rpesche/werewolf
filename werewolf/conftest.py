9872d10005cef364284e4ada171ec01af3db4ae4
Romain Pesche <romain@pesche.fr>
2020-04-18 19:54:14 +0200

    add test setup with pytest

---
+++ werewolf/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse
+
+from werewolf.models import Game
+
+
+@pytest.fixture
+def user(django_user_model):
+    django_user_model.objects.create_user(username='test_user', password='test_password')
+
+
+@pytest.fixture
+def logged_client(user, client):
+    client.login(username='test_user', password='test_password')
+    yield client
+
+
+@pytest.fixture
+def game(logged_client):
+    game_name = 'New Game'
+    url = reverse('new-game')
+    logged_client.post(url, data={'name': game_name})
+    yield Game.objects.get(name=game_name)
